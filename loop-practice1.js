1831321b98b1d31a7b40b40678e4d1b2ccd4c6cd
Student <student@RU-005.local>
2016-01-18 11:13:50 -0700

    first commit

---
+++ loop-practice1.js
@@ -0,0 +1,31 @@
+for (i = 0; i <= 500; i += 100) {
+	console.log(i);
+};
+
+for (i = 1; i <= 64; i *= 2) {
+	console.log(i);
+};
+
+for (i = 1; i <= 3; i += 1) {
+	console.log(i);
+	console.log(i);
+	console.log(i);
+};
+
+for (i = 0; i <= 10; i += 2) {
+	console.log(i);
+};
+
+for (i = 3; i <= 15; i += 3) {
+	console.log(i);
+};
+
+for (i = 9; i >= 0; i -= 1) {
+	console.log(i);
+};
+
+for (i = 0; i < 3; i += 1) {
+	for (n = 0; n <= 3; n += 1) {
+		console.log(n);
+	};
+};
